91e2329c8c175cf6d12f4eb43fe4b4d14cf922d0
olive <tasbiul@mirinfosys.com>
2025-02-15 22:34:00 +0600

    change

--- src/components/LandingPage/Banner1.jsx
+++ src/components/LandingPage/Banner1.jsx
@@ -15,8 +15,8 @@ const Banner1 = ({dataToBeSent}) => {
         </div>
         <div>
           <div className="grid grid-cols-2 lg:grid-cols-1 gap-5 w-full">
-          <img className=' w-full' src='/img/flash-sale/14.webp' alt="Slide 1" />
-          <img className=' w-full' src='/img/flash-sale/15.webp' alt="Slide 1" />
+          <img className='w-full col-span-1' src='/img/flash-sale/14.webp' alt="Slide 1" />
+          <img className='w-full col-span-1' src='/img/flash-sale/15.webp' alt="Slide 1" />
           </div>
         </div>
       </div>
